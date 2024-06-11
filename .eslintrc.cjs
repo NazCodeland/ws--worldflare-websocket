3352b3f7da149b233469b4226484600d61671a06
Nazar <113494366+NazCodeland@users.noreply.github.com>
2024-06-11 14:25:41 -0400

    fix: download eslint packages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,23 +1,19 @@
 /** @type { import("eslint").Linter.Config } */
 module.exports = {
-	root: true,
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'prettier'
-	],
-	rules: {
-		'@typescript-eslint/no-namespace': 'off'
-	},
-	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint'],
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020
-	},
-	env: {
-		browser: true,
-		es2017: true,
-		node: true
-	}
+  root: true,
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  rules: {
+    '@typescript-eslint/no-namespace': 'off',
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true,
+  },
 };
