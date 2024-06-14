23437eb9f04d742fb340ddc113401256f16273ba
Nazar <113494366+NazCodeland@users.noreply.github.com>
2024-06-13 20:25:09 -0400

    refactor: add type to connectedUsers map object

--- src/connectedUsers.ts
+++ src/connectedUsers.ts
@@ -1,3 +1,3 @@
 // a map of all the connected users connected to the WebSocket Server.
 // the 'key' value represents the socket connection and the 'value' represents the unique ID
-export const connectedUsers = new Map();
+export const connectedUsers: Map<WebSocket, number> = new Map();
