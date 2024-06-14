30ffcd235f6922f018bc0908fcf443a4dd1d29af
Nazar <113494366+NazCodeland@users.noreply.github.com>
2024-06-13 20:28:25 -0400

    refactor: modify broadcast funciton to use the connectedUsers map instead of the built in 'ws.clients'

--- src/wsUtilities.ts
+++ src/wsUtilities.ts
@@ -1,20 +1,21 @@
 import { WebSocket } from 'ws';
 import { encode } from './lib/worldflare-shared/codec.js';
 import type { Worldflare } from './lib/worldflare-shared/types.js';
-import { ws } from './websocket.js';
+import { connectedUsers } from './connectedUsers.js';
 
-export function send(message: Worldflare.App.Message, client: WebSocket) {
+export function send(message: Worldflare.App.Message, socket: WebSocket) {
   try {
-    client.send(encode(message));
+    socket.send(encode(message));
   } catch (error) {
     console.log('Error in send function, wsUtilities.ts, a server file');
   }
 }
 
 export function broadcast(message: Worldflare.App.Message) {
-  ws.clients.forEach((client) => {
-    if (client.readyState === WebSocket.OPEN) {
-      send(message, client);
+  connectedUsers.forEach((uniqueId, socket) => {
+    // send to everyone but the current sender
+    if (uniqueId !== message.wsConnId) {
+      send(message, socket);
     }
   });
 }
